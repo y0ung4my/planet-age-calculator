010ad9d0de5cdfccb74e27575a839d14f752aa4d
Amy Young <youngamy1223@gmail.com>
2021-12-27 22:27:37 -0700

    add failing test for planetAges()

--- _tests_/planetages.test.js
+++ _tests_/planetages.test.js
@@ -3,7 +3,17 @@ import Age from '../src/js/planetages.js';
 describe('Age', () => {
 
   test('should create an object for age in Earth years', () => {
-  const age = new Age(30);
-  expect(age.earthAge).toEqual(30);
+    const age = new Age(30);
+    expect(age.earthAge).toEqual(30);
   })
+
+  test('should convert age in Earth years to age on Mercury, Venus, Mars, and Jupiter', () => {
+    const age = new Age(30);
+    age.planetAges();
+    expect(age.mercuryAge).toEqual(7);
+    expect(age.venusAge).toEqual(18);
+    expect(age.marsAge).toEqual(56);
+    expect(age.jupiterAge).toEqual(355);
+  })
+
 })
