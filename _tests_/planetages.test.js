870bbfadf73aedd1a3d2f5c59ed83586c8566efb
Amy Young <youngamy1223@gmail.com>
2021-12-27 21:26:45 -0700

    add a failing first test for Age

--- _tests_/planetages.test.js
+++ _tests_/planetages.test.js
@@ -1,1 +1,9 @@
-import Age from '..src/js/planetage.js';
+import Age from '../src/js/planetages.js';
+
+describe('Age', () => {
+
+  test('should create an object for age in Earth years', () => {
+  const age = new Age(30);
+  expect(age.earthAge).toEqual(30);
+  })
+})
