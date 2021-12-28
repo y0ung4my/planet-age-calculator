a76a5cfe97b2d74470990054a6e27a9a3dee919f
Amy Young <youngamy1223@gmail.com>
2021-12-27 22:42:55 -0700

    pass test for planetAges()

--- src/js/planetages.js
+++ src/js/planetages.js
@@ -3,4 +3,11 @@ export default class Age {
   this.earthAge = earthAge;
 }
 
+planetAges() {
+  this.mercuryAge = Math.floor(this.earthAge * .24);
+  this.venusAge = Math.floor(this.earthAge * .62);
+  this.marsAge = Math.floor(this.earthAge * 1.88);
+  this.jupiterAge = Math.floor(this.earthAge * 11.86);
+}
+
 }
