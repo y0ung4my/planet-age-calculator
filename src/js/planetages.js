d4301df869fd7f5ec47334395e96bd671e6796e4
Amy Young <youngamy1223@gmail.com>
2021-12-28 20:35:52 -0700

    correct calculations for planetAges(), passing test

--- src/js/planetages.js
+++ src/js/planetages.js
@@ -7,10 +7,10 @@ export default class Age {
 }
 
   planetAges() {
-    this.mercuryAge = Math.floor(this.earthAge * .24);
-    this.venusAge = Math.floor(this.earthAge * .62);
-    this.marsAge = Math.floor(this.earthAge * 1.88);
-    this.jupiterAge = Math.floor(this.earthAge * 11.86);
+    this.mercuryAge = Math.floor(this.earthAge / .24);
+    this.venusAge = Math.floor(this.earthAge / .62);
+    this.marsAge = Math.floor(this.earthAge / 1.88);
+    this.jupiterAge = Math.floor(this.earthAge / 11.86);
   }
 
   lifeExpectancy() {
