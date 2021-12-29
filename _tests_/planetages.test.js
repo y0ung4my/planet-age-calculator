173405c50ec2621699f43ecd7737a3808adf60ac
Amy Young <youngamy1223@gmail.com>
2021-12-28 18:15:08 -0700

    fail test for life expectancy factors in Age constructor

--- _tests_/planetages.test.js
+++ _tests_/planetages.test.js
@@ -3,8 +3,11 @@ import Age from '../src/js/planetages.js';
 describe('Age', () => {
 
   test('should create an object for age in Earth years', () => {
-    const age = new Age(30);
+    const age = new Age(30, "no", "yes", "yes");
     expect(age.earthAge).toEqual(30);
+    expect(age.stress).toEqual("no");
+    expect(age.exercise).toEqual("yes");
+    expect(age.vegetables).toEqual("yes");
   })
 
   test('should convert age in Earth years to age on Mercury, Venus, Mars, and Jupiter', () => {
