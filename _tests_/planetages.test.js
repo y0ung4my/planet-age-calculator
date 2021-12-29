bfcacf2406030bee495171ab965a3afdd7876a3f
Amy Young <youngamy1223@gmail.com>
2021-12-28 19:23:58 -0700

    add failing test for stress calculation in lifeExpectancy()

--- _tests_/planetages.test.js
+++ _tests_/planetages.test.js
@@ -20,9 +20,15 @@ describe('Age', () => {
   })
 
     test('should subtract age in Earth years to number of years left based on average life expectancy', () => {
-      const age = new Age(30);
+      const age = new Age(30,);
       age.lifeExpectancy();
       expect(age.yearsLeft).toEqual(48);
+    })
+
+    test('should subtract 2 years plus age Earth years', () => {
+      const age = new Age(30, "yes");
+      age.lifeExpectancy();
+      expect(age.yearsLeft).toEqual(46);
     })
 
 })
